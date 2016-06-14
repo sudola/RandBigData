8fb897fac504d2031e96415572ac909106773d4c
gradzielr <eja.gradziel@gmail.com>
2016-06-14 23:46:41 +0200

    Add files via upload

---
+++ MINI_2016/zespolyP2/MUF/faza3/banki/R/daty_ING.R
@@ -0,0 +1,10 @@
+#' Zbior danych daty_ING
+#'
+#' Zbior zawiera daty kazdego wpisu ramki wpisy_ING
+#'
+#' @docType data
+#' @keywords datasets
+#' @name daty_ING
+#' @usage data(daty_ING)
+#' @format wektor dat dlugosci 6977
+NULL
---
+++ MINI_2016/zespolyP2/MUF/faza3/banki/R/nouns_ALIOR.R
@@ -0,0 +1,10 @@
+#' Zbior danych nouns_ALIoR
+#'
+#' Zbior zawiera rzeczowniki w formie podstawowej z kazdego wpisu z ramki wpisy_ALIOR
+#'
+#' @docType data
+#' @keywords datasets
+#' @name nouns_ALIOR
+#' @usage data(nouns_ALIOR)
+#' @format lista wektorow napisow dlugosci 6920
+NULL
